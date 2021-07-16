865a5d4e4e8f0e7b8e8d256a8677be385ca3171e
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-07-16 13:50:19 +0200

    quarked

--- src/main/java/com/redhat/atlasmap/operator/AtlasMapController.java
+++ src/main/java/com/redhat/atlasmap/operator/AtlasMapController.java
@@ -1,5 +1,7 @@
 package com.redhat.atlasmap.operator;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,21 +12,19 @@ import io.javaoperatorsdk.operator.api.DeleteControl;
 import io.javaoperatorsdk.operator.api.ResourceController;
 import io.javaoperatorsdk.operator.api.UpdateControl;
 
-@Controller
+@Controller(name = "atlasmap")
 public class AtlasMapController implements ResourceController<AtlasMap> {
     static final String USERNAME_FORMAT = "%s-user";
     static final String SECRET_FORMAT = "%s-secret";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
-    private final KubernetesClient kubernetesClient;
 
-    public AtlasMapController(KubernetesClient kubernetesClient) {
-        this.kubernetesClient = kubernetesClient;
-    }
+    @Inject
+    KubernetesClient kubernetesClient;
 
     @Override
     public UpdateControl<AtlasMap> createOrUpdateResource(AtlasMap resource, Context<AtlasMap> context) {
-        // TODO 
+        // TODO
 
         return null;
     }
@@ -41,5 +41,4 @@ public class AtlasMapController implements ResourceController<AtlasMap> {
             return DeleteControl.NO_FINALIZER_REMOVAL;
         }
     }
-
 }
--- src/main/java/com/redhat/atlasmap/operator/AtlasMapOperator.java
+++ src/main/java/com/redhat/atlasmap/operator/AtlasMapOperator.java
@@ -2,32 +2,29 @@ package com.redhat.atlasmap.operator;
 
 import java.io.IOException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.takes.facets.fork.FkRegex;
-import org.takes.facets.fork.TkFork;
-import org.takes.http.Exit;
-import org.takes.http.FtBasic;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 
-import io.fabric8.kubernetes.client.Config;
-import io.fabric8.kubernetes.client.ConfigBuilder;
-import io.fabric8.kubernetes.client.DefaultKubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
-import io.javaoperatorsdk.operator.config.runtime.DefaultConfigurationService;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
 
+@ApplicationScoped
 public class AtlasMapOperator {
-    private static final Logger log = LoggerFactory.getLogger(AtlasMapOperator.class);
 
-    public static void main(String[] args) throws IOException {
-        log.info("MySQL Schema Operator starting");
+    @Inject
+    Operator operator;
 
-        Config config = new ConfigBuilder().withNamespace(null).build();
-        KubernetesClient client = new DefaultKubernetesClient(config);
-       
-        Operator operator = new Operator(client, DefaultConfigurationService.instance());
-        operator.register(new AtlasMapController(client));
+    void onStart(@Observes StartupEvent ignored) {
+        operator.start();
+    }
 
-        new FtBasic(new TkFork(new FkRegex("/health", "ALL GOOD!")), 8080).start(Exit.NEVER);
+    void onStop(@Observes ShutdownEvent ignored) {
+        operator.close();
+    }
+
+    public static void bar() throws IOException {
+        //new FtBasic(new TkFork(new FkRegex("/health", "ALL GOOD!")), 8080).start(Exit.NEVER);
     }
 }
