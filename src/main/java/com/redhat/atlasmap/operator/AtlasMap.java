65ccf782bcd1d187b0c7ee61a0bdf40bdf9c8f70
Luca Burgazzoli <lburgazzoli@gmail.com>
2021-07-16 13:50:19 +0200

    quarked

--- src/main/java/com/redhat/atlasmap/operator/AtlasMap.java
+++ src/main/java/com/redhat/atlasmap/operator/AtlasMap.java
@@ -3,9 +3,13 @@ package com.redhat.atlasmap.operator;
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.ShortNames;
 import io.fabric8.kubernetes.model.annotation.Version;
 
-@Group("atlasmap.javaoperatorsdk")
-@Version("v1")
+@Group(AtlasMap.GROUP)
+@Version(AtlasMap.VERSION)
+@ShortNames("amap")
 public class AtlasMap extends CustomResource<AtlasMapSpec, AtlasMapStatus> implements Namespaced {
+    public static final String VERSION = "v1alpha1";
+    public static final String GROUP = "atlasmap.javaoperatorsdk";
 }
--- src/main/java/com/redhat/atlasmap/operator/AtlasMapList.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.atlasmap.operator;
-
-import io.fabric8.kubernetes.client.CustomResourceList;
-
-public class AtlasMapList extends CustomResourceList<AtlasMap> {
-    
-}
